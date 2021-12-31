020e9cd6e798a870a2d461b54bf3eca2877ffe0c
Curto <572994+scurtoni@users.noreply.github.com>
2021-12-31 12:11:03 +0100

    test

--- src/components/Debug.tsx
+++ src/components/Debug.tsx
@@ -10,7 +10,7 @@ const Debug = () => {
         process.env.PRODUCTION: <b>{process.env.PRODUCTION.toString()}</b>
       </p>
       <p>
-        process.env.NAME: <b>{process.env.NAME}</b>
+        process.env.NAME:<b>{process.env.NAME}</b>
       </p>
       <p>
         process.env.VERSION: <b>{process.env.VERSION}</b>
